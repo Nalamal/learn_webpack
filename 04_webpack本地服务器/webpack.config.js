af1e90d5ce8d10e6825e307b6cf4226f52acb5b9
贺煜林 <1981766322@qq.com>
2023-05-23 20:56:09 +0800

    feat: add devServer

---
+++ 04_webpack本地服务器/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: "bundle.js",
+    // 重新打包时，先将之前打包的文件夹删除掉
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".json", ".wasm", ".jsx", ".ts"],
+  },
+  devServer: {
+    static: ["public", "content"],
+    host: "0.0.0.0",
+    port: 8888,
+    open: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      // 针对jsx?代码进行babel处理
+      {
+        test: /\.jsx?$/, // x?: 0或者1个x
+        // exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.ts$/,
+        // use: "ts-loader",
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
